6b221f0a52a78595ad6e81135a111432f5dcacb7
yatharth-gupta <102593041+yatharth-gupta@users.noreply.github.com>
2022-09-05 22:50:18 +0530

    Add files via upload

--- assignment-2/pinfo.c
+++ assignment-2/pinfo.c
@@ -17,6 +17,31 @@
 #include "ls.h"
 #include "variables.h"
 #include "pinfo.h"
+void find_exe_tilda(char *currunt_dir, char *relative_curr_dir)
+{
+    int i = 0;
+    int a = strlen(currunt_dir);
+    int b = strlen(tilda);
+    if (a < b)
+    {
+        strcpy(relative_curr_dir, currunt_dir);
+    }
+    while (currunt_dir[i] == tilda[i] && i < b)
+    {
+        i++;
+    }
+    if (i != b)
+    {
+        strcpy(relative_curr_dir, currunt_dir);
+    }
+    else if (a == b)
+    {
+    }
+    else
+    {
+        strcat(&relative_curr_dir[1], &currunt_dir[i]);
+    }
+}
 void pinfo__(char *tokens[], int token_size)
 {
     if (token_size > 2)
@@ -24,34 +49,102 @@ void pinfo__(char *tokens[], int token_size)
         printf("too many arguments\n");
         return;
     }
-    pid_t pid_value;
     if (token_size == 1)
     {
-        pid_value = getpid();
+        int pid = getpid();
+        printf("pid : %d\n", pid);
+        char buf[1000];
+        sprintf(buf, "/proc/%d/stat", pid);
+        FILE *fd = fopen(buf, "r");
+        char number[100];
+        for (int i = 0; i < 3; ++i)
+        {
+            fscanf(fd, "%s", number);
+        }
+        pid_t console_pid = tcgetpgrp(STDOUT_FILENO);
+        pid_t my_pid = getpgrp();
+        if (console_pid == my_pid)
+            printf("Process Status : %s+\n", number);
+        else
+            printf("Process Status : %s\n", number);
+        char buf1[1000];
+        sprintf(buf1, "/proc/%d/status", pid);
+        FILE *fd1 = fopen(buf1, "r");
+        bool g = false;
+        char memory[100];
+        while (strcmp("VmSize:", memory) != 0)
+        {
+            if (fscanf(fd1, "%s", memory) == -1)
+            {
+                g = true;
+                break;
+            }
+        }
+        if (!g)
+        {
+            fscanf(fd1, "%s", memory);
+        }
+        else
+        {
+            strcpy(memory, "0");
+        }
+        printf("Virtual Memory : %s\n", memory);
+        char exe[1000];
+        char rel_exe[1000] = "~";
+        int read = readlink("/proc/self/exe", exe, 1000);
+        exe[read] = '\0';
+        find_exe_tilda(exe, rel_exe);
+        // printf("%s\n",tilda);
+        printf("%s\n", rel_exe);
     }
     else
     {
-        pid_value = atoi(tokens[1]);
+        int pid = atoi(tokens[1]);
+        printf("pid : %d\n", pid);
+        char buf[1000];
+        sprintf(buf, "/proc/%d/stat", pid);
+        FILE *fd = fopen(buf, "r");
+        char number[100];
+        for (int i = 0; i < 3; ++i)
+        {
+            fscanf(fd, "%s", number);
+        }
+        pid_t console_pid = tcgetpgrp(STDOUT_FILENO);
+        pid_t my_pid = getpgrp();
+        if (console_pid == my_pid)
+            printf("Process Status : %s+\n", number);
+        else
+            printf("Process Status : %s\n", number);
+        char buf1[1000];
+        sprintf(buf1, "/proc/%d/status", pid);
+        FILE *fd1 = fopen(buf1, "r");
+        bool g = false;
+        char memory[100];
+        while (strcmp("VmSize:", memory) != 0)
+        {
+            if (fscanf(fd1, "%s", memory) == -1)
+            {
+                g = true;
+                break;
+            }
+        }
+        if (!g)
+        {
+            fscanf(fd1, "%s", memory);
+        }
+        else
+        {
+            strcpy(memory, "0");
+        }
+        printf("Virtual Memory : %s\n", memory);
+        char exe[1000];
+        char rel_exe[1000] = "~";
+        char buf5[1000];
+        sprintf(buf5, "/proc/%d/exe", pid);
+        int read = readlink(buf5, exe, 1000);
+        exe[read] = '\0';
+        find_exe_tilda(exe, rel_exe);
+        // printf("%s\n",tilda);
+        printf("%s\n", rel_exe);
     }
-    printf("pid : %d\n", pid_value);
-    char status_path[1000];
-    char buffer[1000];
-    sprintf(status_path, "/proc/%d/status", pid_value);
-    FILE *file_status = fopen(status_path, "r");
-    char status;
-    long unsigned memory;
-    if (file_status)
-    {
-        // printf("%s ", file_status);
-        // fgets(buffer, 1000, file_status);
-        // // printf("%s", buffer);
-        // sscanf(buffer, "State:\t%c", &state);
-        // printf("Process Status : %c\n", state);
-        // fscanf(file_status, "%d %*s %c %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %lu %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d %*d", &pid_value, &status, &memory);
-        // fclose(file_status);
-        // printf("pid -- %d\n", pid_value);
-        // printf("Process Status -- %c\n", status);
-        // printf("Memory -- %lu\n", memory);
-    }
-    
 }
--- assignment-2/process.c
+++ assignment-2/process.c
@@ -9,8 +9,6 @@ void bg__(char *tokens[], int token_size)
 {
     // puts("hi\n");
     tokens[token_size] = (char *)NULL;
-    // puts("hi\n");
-    // printf("%s\n", tokens[token_size]);
     pid_t pid_value = fork();
     if (pid_value < 0)
     {
@@ -27,29 +25,12 @@ void bg__(char *tokens[], int token_size)
     {
         printf("[%d] %d\n", ++x, pid_value);
         pids[x-1] = pid_value;
-        // printf("%d\n", pids[pid_value]);
-        // signal(SIGCHLD)
-        // // if (SIGCHLD)
-        // // {
-        // //     printf("hi");
-        // // }
-
-        // int status;
-        // wait(&status);
-        // // printf("%d\n", pid_value);
-        // if (WIFEXITED(status))
-        // {
-        //     int exit_status = WEXITSTATUS(status);
-        //     printf("Exit status of the child was %d\n", exit_status);
-        // }
-        // parent does not wait
     }
 }
 int fg__(char *tokens[], int token_size)
 {
     int wstatus;
     tokens[token_size] = NULL;
-    // printf("%s\n", tokens[token_size]);
     pid_t pid_value = fork();
     if (pid_value < 0)
     {
@@ -61,11 +42,8 @@ int fg__(char *tokens[], int token_size)
         if (execvp(tokens[0], tokens) == -1)
         {
             printf("execvp() error\n");
-            // fprintf(stderr, "execvp() failed!\n");
             return -1;
         }
-        // fprintf(stderr, "execvp() failed1!\n");
-        // printf("%s with pid = %d")
     }
     else
     {
@@ -75,16 +53,6 @@ int fg__(char *tokens[], int token_size)
             return -1;
         }
         pid_fg = pid_value;
-        // signal(SIGCHLD, SIG_IGN);
-        // if (wait(&wstatus) == -1)
-        // {
-        //     printf("waitpid() error");
-        //     return -1;
-        // }
-        // else
-        // {
-        //     printf("%d\n", wstatus);
-        // }
     }
     return 1;
 }
